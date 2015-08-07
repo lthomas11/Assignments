8b006b8c1c46b943cddf1ccfb59224af4f3157f7
lthomas11 <lthomas1@fullsail.edu>
2015-08-06 23:58:25 -0400

    Thomas_Leonard_Output_Assignment

---
+++ js/Thomas_Leonard_Output_Assignment.js
@@ -0,0 +1,16 @@
+alert("Since you don't know me, here are some questions that I can answer.");
+alert("Could you guess what state I'm from?");
+prompt("Take a good guess.  Hint: It can get pretty rocky where I\'m from...");
+var myState = Tennessee
+prompt(myState)
+alert("I have a family.");
+prompt("Can you guess what year my son was born?");
+prompt("Enter answer here");
+var sonByear = 2007;
+prompt(sonByear)
+alert("Do you like riddles?");
+alert("I have a riddle for you...");
+alert("You can always see me execpt in the light, what am I?");
+prompt("Enter answer here");
+var riddleAnswer = darkness;
+concatString = riddleAnswer;
