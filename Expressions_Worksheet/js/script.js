a90fa7b8b478e86c9a3d8b43de38435676642651
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-12 19:48:03 -0400

    Comments and to-dos made for script

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -8,9 +8,14 @@ prompt("JavaScript works!!!");
 
 //variables
 //TODO create variables
+//var sparkysAge
+//var sparkysAge = humanAge * 7
+//var humanAge = prompt
 
 //prompts
 //TODO create prompts
+//prompt = "Enter your age here..."
 
 //outputs
 //TODO create outputs
+//Sparky is X human years old which is X in dog years.
