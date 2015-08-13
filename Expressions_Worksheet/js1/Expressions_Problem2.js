80cb29c1b52e85946d7e6bf373ce5e06518d90e1
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-12 20:50:47 -0400

    Problem 2 JS and HTML
    Set up and coding for both js and html file for problem 2

---
+++ Expressions_Worksheet/js1/Expressions_Problem2.js
@@ -0,0 +1,25 @@
+/*
+ Leonard Thomas
+ SDI 150812
+ Expressions Worksheet
+ */
+//variables
+//TODO create variables
+//var pizzaSlices
+//var partyPeople
+//var total slices
+var pizzaSlices;
+var partyPeople;
+var totalSlices;
+var totalPies;
+//prompts
+//TODO create prompts
+//prompt = "Enter your age here..."
+pizzaSlices = prompt("Enter how many slices of pizza...");
+partyPeople = prompt("Enter how many people are at the event...");
+totalPies = prompt("Enter how many pizzas were ordered...")
+totalSlices = totalPies / partyPeople / pizzaSlices;
+//outputs
+//TODO create outputs
+alert("Each person ate " + totalSlices + " slices of pizza at the party.");
+console.log("Each person ate " + totalSlices + " slices of pizza at the party.");
