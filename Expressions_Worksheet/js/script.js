df7ed029561b1cb838f600176007fb20050a2c22
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-12 20:07:52 -0400

    Added variables and prompts

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -3,19 +3,18 @@ Leonard Thomas
 SDI 150812
 Expressions Worksheet
  */
-
 prompt("JavaScript works!!!");
-
 //variables
 //TODO create variables
 //var sparkysAge
 //var sparkysAge = humanAge * 7
 //var humanAge = prompt
-
+var sparkysAge = humanAge * 7;
 //prompts
 //TODO create prompts
 //prompt = "Enter your age here..."
-
+humanAge = prompt("Enter your age here...");
 //outputs
 //TODO create outputs
 //Sparky is X human years old which is X in dog years.
+console.log("You are "+ humanAge);
