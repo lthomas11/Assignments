a3b222968e366458c20f612466ab2af1d0f5e6ac
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-12 20:24:01 -0400

    Additions to script

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -3,18 +3,21 @@ Leonard Thomas
 SDI 150812
 Expressions Worksheet
  */
-prompt("JavaScript works!!!");
+//prompt("JavaScript works!!!");
 //variables
 //TODO create variables
 //var sparkysAge
 //var sparkysAge = humanAge * 7
 //var humanAge = prompt
-var sparkysAge = humanAge * 7;
+var sparkysAge;
+var humanAge;
 //prompts
 //TODO create prompts
 //prompt = "Enter your age here..."
 humanAge = prompt("Enter your age here...");
+sparkysAge = humanAge * 7;
 //outputs
 //TODO create outputs
+alert("Sparky is " + humanAge + " human years old, which is " + sparkysAge + " in dog years.");
 //Sparky is X human years old which is X in dog years.
-console.log("You are "+ humanAge);
+console.log("Sparky is " + humanAge + " human years old, which is " + sparkysAge + " in dog years.");
