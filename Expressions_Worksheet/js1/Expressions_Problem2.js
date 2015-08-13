adeb58242d50fbef128f6f41bc9ce05e4b1aaa61
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-12 21:13:21 -0400

    Additions to js
    Added Sparky’s take and leftovers

--- Expressions_Worksheet/js1/Expressions_Problem2.js
+++ Expressions_Worksheet/js1/Expressions_Problem2.js
@@ -12,14 +12,19 @@ var pizzaSlices;
 var partyPeople;
 var slicesPer;
 var totalPies;
+var sparkysKeep;
+var leftOvers;
 //prompts
 //TODO create prompts
 //prompt = "Enter your age here..."
 partyPeople = prompt("Enter how many people are at the event...");
 totalPies = prompt("Enter how many pizzas were ordered...");
 pizzaSlices = prompt("Enter how many slices of pizza...");
+leftOvers = prompt("Enter how many slices are left over...")
 slicesPer = totalPies / pizzaSlices * partyPeople;
+sparkysKeep = slicesPer - leftOvers
 //outputs
 //TODO create outputs
 alert("Each person ate " + slicesPer + " slices of pizza at the party.");
+alert("Sparky can eat " + sparkysKeep + " slices of pizza!")
 console.log("Each person ate " + slicesPer + " slices of pizza at the party.");
