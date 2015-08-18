4e38c6d2c547733850cf4d230acb9d40b736b8b5
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-17 21:51:44 -0400

    Initial process for Conditionals Assignment

---
+++ Thomas_Leonard_Conditionals_Assignment/script.js
@@ -0,0 +1,21 @@
+/**
+ Leonard Thomas
+ 8/17/15
+ SDI150817
+ */
+// my variables
+var magicKingdom;
+var waterPark;
+var hotelRoom;
+var epcotCenter;
+
+//input from user
+prompt("Where would you like to go?" + "  Magic Kingdom" + ", Water Park" + ", Epcot Center?");
+prompt("Is it raining?  Select Ok for yes and cancel for no.");
+
+//my outputs
+
+//Boolean conditional
+
+
+//code to run if "construction" is true
