c8cbfeb87e5d76f8f6b59138c8c5d762f6a38d8a
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-19 23:46:39 -0400

    Changes and comments to assignment

--- Thomas_Leonard_Conditionals_Assignment/script.js
+++ Thomas_Leonard_Conditionals_Assignment/script.js
@@ -4,18 +4,39 @@
  SDI150817
  */
 // my variables
-var magicKingdom;
-var waterPark;
-var hotelRoom;
-var epcotCenter;
+
+var awesomePark  = "Epcot Center";
+var isRaining    = false;
+var todaysTemp   = 70;
+var comfyTemp    = 80;
+
 
 //input from user
-prompt("Where would you like to go?" + "  Magic Kingdom" + ", Water Park" + ", Epcot Center?");
-prompt("Is it raining?  Select Ok for yes and cancel for no.");
+alert("Let's go to " + awesomePark + " today!!!");//Informs user of situation
+
+//my confirmations
+comfyTemp = confirm("Is it greater or less than 80 degrees fahrenheit outside?  (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
+isRaining   = confirm("Is it raining? (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
 
 //my outputs
+console.log("I would like to go to " + awesomePark + " today!!!");//Informs user of decision
+console.log("It is currently " + comfyTemp + " degrees fahrenheit outside.");//
+
 
 //Boolean conditional
+if (isRaining !== true) {
+} else {
 
+    //code to run if "raining" is true
+    console.log("It is " + isRaining + " that is is raining today.");
+    if (todaysTemp < comfyTemp);
+    console.log("Then, we will not go to " + awesomePark + " today...");
+}
 
-//code to run if "construction" is true
+if (isRaining === false) {
+
+    //code to run if "raining" is false
+    console.log("It is " + isRaining + " that is is raining today.");
+    if (todaysTemp > comfyTemp);
+    console.log("Then, we'll go out and wear summer clothes to " + awesomePark + " today!!!");
+}
