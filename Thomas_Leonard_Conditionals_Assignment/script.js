e71de0a8c2c3ddf65556d0a9ce2b9a2ff211fb67
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-20 23:58:16 -0400

    Conditionals Assignment
    Final changes and submission to repository…  Wish me luck!!!

--- Thomas_Leonard_Conditionals_Assignment/script.js
+++ Thomas_Leonard_Conditionals_Assignment/script.js
@@ -8,15 +8,21 @@
 var awesomePark  = "Epcot Center";//Variable used to describe Epcot Center
 var isRaining    = false;//Variable to determine weather status
 var comfyTemp    = 70;//Variable used to set comfortable temp
-var userInput    = prompt("Please type in today's temperature.");//Variable used to request input from user
+var totalBudget  = 150;
+var ticketMoney  = 100;
+var discStudent  = ticketMoney * .15;
 
-//input from user
+
+//info to user
 alert("Let's go to " + awesomePark + " today!!!");//Informs user of situation
+var userInput    = prompt("Please type in today's temperature.");//Variable used to request input from user
+alert("You have a total budget of $" + totalBudget + " to buy your ticket and gifts.");//Informs user of situation
+var spendingMoney  = prompt("Please type in how much is left after buying your ticket.  (Your ticket is $100.)");
 
 //my confirmations
-//Requesting true/false response from user
 isRaining   = confirm("Is it raining? (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
-
+discStudent = confirm("Do you have a student discount?  (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
+spendingMoney = confirm("Do you have enough for souvenirs?  (Select Ok for yes and Cancel for no.)");
 //my outputs
 console.log("I would like to go to " + awesomePark + " today!!!");//Informs user of decision
 console.log("It is currently " + userInput + " degrees fahrenheit outside.");//Informs user of decision
@@ -38,4 +44,15 @@ if (isRaining === true) {
     //code to run if "raining" is false
     console.log("It is " + isRaining + " that is is raining today.");//Information for user to receive
     console.log("Then we should go to " + awesomePark + " today!!!");//Decision path #4 for solution
+}
+
+//Boolean conditional #3
+(discStudent === true) ?
+    console.log("Since you have a student discount, you save $15 on your ticket."):console.log("If you had a student discount, you could have saved $15 on your ticket.  Sign up with Full Sail University today!!!");
+
+//Boolean conditional #4
+if (spendingMoney === true){
+    console.log("Let's get those Micky Mouse ears!!!");
+}else{
+    console.log("You have to save up for next time!!!");
 }
