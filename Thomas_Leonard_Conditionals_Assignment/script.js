c9dce77ac105cf26bdb31e62eefb50d183db1540
Leonard Thomas <leonardthomas68@gmail.com>
2015-08-20 21:32:36 -0400

    Conditionals Assignment
    Changes to script.js (comments, edits, and additions)

--- Thomas_Leonard_Conditionals_Assignment/script.js
+++ Thomas_Leonard_Conditionals_Assignment/script.js
@@ -7,36 +7,34 @@
 
 var awesomePark  = "Epcot Center";
 var isRaining    = false;
-var todaysTemp   = 70;
-var comfyTemp    = 80;
+var todaysTemp   = 85;
+var comfyTemp    = 75;
 
 
 //input from user
 alert("Let's go to " + awesomePark + " today!!!");//Informs user of situation
 
 //my confirmations
-comfyTemp = confirm("Is it greater or less than 80 degrees fahrenheit outside?  (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
+comfyTemp = confirm("Is it greater or less than 75 degrees fahrenheit outside?  (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
 isRaining   = confirm("Is it raining? (Select Ok for yes and Cancel for no.)");//Requesting true/false response from user
 
 //my outputs
 console.log("I would like to go to " + awesomePark + " today!!!");//Informs user of decision
-console.log("It is currently " + comfyTemp + " degrees fahrenheit outside.");//
+console.log("It is currently " + todaysTemp + " degrees fahrenheit outside.");//
 
 
 //Boolean conditional
-if (isRaining !== true) {
-} else {
+if (isRaining === true) {
 
     //code to run if "raining" is true
     console.log("It is " + isRaining + " that is is raining today.");
-    if (todaysTemp < comfyTemp);
     console.log("Then, we will not go to " + awesomePark + " today...");
-}
-
-if (isRaining === false) {
+}else{
 
     //code to run if "raining" is false
     console.log("It is " + isRaining + " that is is raining today.");
-    if (todaysTemp > comfyTemp);
-    console.log("Then, we'll go out and wear summer clothes to " + awesomePark + " today!!!");
+}
+
+if (comfyTemp >= todaysTemp){
+    console.log("Let's bring a jacket to " + awesomePark + ".");
 }
